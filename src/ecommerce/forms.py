fcec168a014a35415471c0067610980e96fd7e0a
kittisitthichai <kittisitthichai30084@gmail.com>
2019-04-16 16:11:34 +0700

    add custom login logout

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.models import User
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField()
+    email = forms.EmailField()
+    content = forms.CharField()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email    = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirn password' ,widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("username is taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is taken")
+        return email
+
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password2 != password:
+            raise forms.ValidationError("Passwords must match.")
+        return data
--- src/ecommerce/view.py
+++ src/ecommerce/view.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
+from django.contrib.auth.models import User
 
 def home_page(request):
     context = { 
@@ -8,8 +10,48 @@ def home_page(request):
     }
     return render(request, "home_page.html",context)
 
-def about_page(request):
-    return render(request,"Hello World About page")
-
 def contact_page(request):
-    return render(request,"Hello World Contact Page")
+    context = { 
+        "title" : "Hello Contact page"
+    }
+    return render(request, "contact/view.html",context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = { 
+        "form" : form
+    }
+    context['form'] = LoginForm()
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            #print(request.user.is_authenticated())
+            print(user)
+            print("You are login")
+            login(request, user)
+            #redirect to sucess page
+            #context['form'] = LoginForm()
+            
+            return redirect("/")
+        else:
+            print("Error")
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = { 
+        "form" : form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        print(form.cleaned_data)
+        new_user= User.objects.create_user(username, email, password)
+        print(new_user)
+
+    return render(request, "auth/register.html", context)
